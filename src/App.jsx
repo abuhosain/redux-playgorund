c5c68616b394dcff68e1b7db77fcb1736a6ec337
Hosain <cryptichasan925@gmail.com>
2025-04-26 14:13:46 +0600

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
- 
+import Home from "./pages/Home";
 
 function App() {
-  
-
   return (
-   <div>
-      <h1 className="text-xl text-center">My React App</h1>
-   </div>
-  )
+    <div>
+       <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
